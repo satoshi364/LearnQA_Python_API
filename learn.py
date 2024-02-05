740d3199d364e15cb81e289eac0d5a9c27a7e662
satoshi364 <satoshi364@mail.ru>
2024-02-05 20:38:55 +0300

    Ex5

--- learn.py
+++ learn.py
@@ -1,5 +1,21 @@
 import requests
+import json
 
-url = "https://playground.learnqa.ru/api/get_text"
-response = requests.get(url)
-print(response.text)
+
+json_text = '''
+{
+  "messages": [
+    {
+      "message": "This is the first message",
+      "timestamp": "2021-06-04 16:40:53"
+    },
+    {
+      "message": "And this is a second message",
+      "timestamp": "2021-06-04 16:41:01"
+    }
+  ]
+}
+'''
+
+parsed_json = json.loads(json_text)
+print(parsed_json)
