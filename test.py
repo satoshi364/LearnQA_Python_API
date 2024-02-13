d47a3b19d4ee04701d6f08a4bb22d741453f5253
satoshi364 <satoshi364@mail.ru>
2024-02-13 20:59:21 +0300

    Ex11

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-def test_input_phrase_length():
-    phrase = input("Set a phrase: ")
-    assert len(phrase) < 15, "Фраза должна быть короче 15 символов"
+import requests
+
+
+def test_homework_cookie():
+    url = "https://playground.learnqa.ru/api/homework_cookie"
+    response = requests.get(url)
+    print(response.cookies)
+
+    cookie_name = "HomeWork"
+    expected_cookie_value = "hw_value"
+
+    assert cookie_name in response.cookies, f"Expected cookie {cookie_name} not found"
+    assert response.cookies[
+               cookie_name] == expected_cookie_value, f"Unexpected value for cookie {cookie_name}: {response.cookies[cookie_name]}"
