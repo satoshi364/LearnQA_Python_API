d0a468a7373c192d058447a520a448ddc8050864
satoshi364 <satoshi364@mail.ru>
2024-02-13 21:24:06 +0300

    Ex12

--- test.py
+++ test.py
@@ -1,14 +1,22 @@
 import requests
+import pytest
 
+def test_homework_header():
+    response = requests.get("https://playground.learnqa.ru/api/homework_header")
+    headers = response.headers
+    print("Headers:", headers)
 
-def test_homework_cookie():
-    url = "https://playground.learnqa.ru/api/homework_cookie"
-    response = requests.get(url)
-    print(response.cookies)
+    expected_values = {
+        'Date': 'Tue, 13 Feb 2024 18:01:19 GMT',
+        'Content-Type': 'text/html; charset=utf-8',
+        'Content-Length': '0',
+        'Connection': 'keep-alive',
+        'Keep-Alive': 'timeout=10',
+        'Server': 'Apache',
+        'Set-Cookie': 'HomeWork=hw_value; expires=Fri, 15-Mar-2024 18:01:19 GMT; Max-Age=2678400; path=/; domain=playground.learnqa.ru; HttpOnly',
+        'Cache-Control': 'max-age=0',
+        'Expires': 'Tue, 13 Feb 2024 18:01:19 GMT'
+    }
 
-    cookie_name = "HomeWork"
-    expected_cookie_value = "hw_value"
-
-    assert cookie_name in response.cookies, f"Expected cookie {cookie_name} not found"
-    assert response.cookies[
-               cookie_name] == expected_cookie_value, f"Unexpected value for cookie {cookie_name}: {response.cookies[cookie_name]}"
+    for header, expected_value in expected_values.items():
+        assert headers.get(header) == expected_value, f"Unexpected value for header {header}: {headers.get(header)}"
